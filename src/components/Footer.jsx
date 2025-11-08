66b6382c0717e5e394d687986ec09643943e83fe
David Huenteleo <davidmauriciohuenteleo@gmail.com>
2025-11-08 17:30:35 -0300

    octay_0.1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     return (
     <nav className="wrapper has-padding">
         <div className="footer">
-            <h1>soy un Footer</h1>
+            <h1>soy un Footr</h1>
         </div>
     </nav>
     )
